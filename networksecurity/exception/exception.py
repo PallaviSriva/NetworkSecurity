686e5232e790e868f7fef9328c651374dee16204
PallaviSriva <sripallavi2580@gmail.com>
2024-12-31 12:21:26 +0530

    Exception handling and logging completed

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.path.insert(0, r'C:\Users\spall\AppData\Local\Programs\Python\Python311\Krish_naik\Projects\NetworkSecurity/')
+from networksecurity.logging import logger
+
+class NetworkSecurityException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+        
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    
+    def __str__(self):
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name, self.lineno, str(self.error_message))
+        
+if __name__=='__main__':
+    try:
+        logger.logging.info("Enter the try block")
+        a=1/0
+        print("This will not be printed",a)
+    except Exception as e:
+           raise NetworkSecurityException(e,sys)
